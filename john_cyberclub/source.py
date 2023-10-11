365b6ad26f70ee728b09287732a3c88f94f88ba5
Thomas Safago <105431546+naeyru@users.noreply.github.com>
2023-10-10 20:15:00 -0500

    Optimized BF function, helper function

--- john_cyberclub/source.py
+++ john_cyberclub/source.py
@@ -5,6 +5,8 @@ Other files may work, but Python may prove to be the most intuitive in this case
 When adding a utility function, add a comment to describe what it does and what it is for.
 '''
 
+import math
+
 
 def str2bf(obj):  # Takes string (or iterable) 'obj' and returns the code for how to output 'obj' in esolang BF as a string.
     prev = 0
@@ -17,6 +19,51 @@ def str2bf(obj):  # Takes string (or iterable) 'obj' and returns the code for ho
                 n += '-' * (prev - curr)
         prev = curr
         n += '.'
+    return n
+
+
+
+def linear_combo(n):  # Relatively optimal means of getting a linear combo + optional i for squares.
+    s = round(math.sqrt(n)) + 1  # Add one to count for squares
+    ret_val = (0, 0, n)
+    prev = n  # Worst case scenario for length representation
+
+    while n % s != 0 or s > 1:  # Case 1: Lower or equal
+        s -= 1
+        a = (n // s)
+        b = n - (s * a)
+        curr = s + a + b
+        if curr < prev:
+            ret_val = (s, a, b)
+            prev = curr
+    return ret_val
+
+
+def str2bf2(obj):  # A more optimized implementation, using more advanced principles to save space / memory
+    n = str()
+    char = ''
+    prev = 0
+
+    for curr in map(ord, obj):
+        if curr > prev:
+            char = '+'
+        else:
+            char = '-'
+
+        first = ""
+        middle = ""
+        last = ""
+
+        l_c = linear_combo(abs(curr - prev))
+
+        if l_c[0] > 0:  # Due to how linear_combo works, l_c[1] has a value if l_c[0] does
+            first = '+' * l_c[0]
+            middle = f"[<{char * l_c[1]}>-]"
+
+        last = char * l_c[2]
+
+        n += f">{first}{middle}<{last}."
+        prev = curr
     return n
 
 
